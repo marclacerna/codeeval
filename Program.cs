dc357e30c41f98e23a83f455fb8393ab425ac086
Marc <marcyvan.lacerna@gmail.com>
2016-08-29 23:47:52 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeEval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string fileInput = "125,1,2";
+            //string multipleInput = "358, 32";
+            string dataRecov = ("programming first The language;3 2 1");
+            TestClass test = new TestClass();
+            //test.MultipleCompare(multipleInput);
+            //test.MultiplicationTable(12);
+            //test.LowerCase("Eqpu'pg%UnT!jo)7V}u5RF*~~-L_2dYV4");
+            test.DataRecovery(dataRecov);
+
+            string filePath = "C:/Users/Marc/Documents/Visual Studio 2015/Projects/CodeEval/input.txt";
+            StreamReader reader = File.OpenText(filePath);
+            while(!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+            }
+        }
+    }
+}
---
+++ TestClass.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeEval
+{
+    class TestClass
+    {
+        public void BitConvert(string line)
+        {
+            string[] subStr = line.Split(',');
+            string result = "";
+            //PrintTest(subStr);
+            int value, pos1, pos2;
+
+            Int32.TryParse(subStr[0], out value);
+            Int32.TryParse(subStr[1], out pos1);
+            Int32.TryParse(subStr[2], out pos2);
+
+            pos1 -= 1;
+            pos2 -= 1;
+
+            if (((value >> pos1) & 1) == ((value >> pos2) & 1))
+            {
+                result = "true";
+            }
+            else
+            {
+                result = "false";
+            }
+
+            Console.WriteLine(result);
+        }
+        private void PrintTest(string[] subStr)
+        {
+            foreach(var x in  subStr)
+            {
+                Console.WriteLine(x);
+            }
+
+        }
+
+        // multiples of a number
+        public void MultipleCompare(string line)
+        {
+            string[] subStr = line.Split(',');
+            int x, n;
+            Int32.TryParse(subStr[0], out x);
+            Int32.TryParse(subStr[1], out n);
+
+            int index = n;
+            while(n < x)
+            {
+                n += index;
+            }
+            Console.WriteLine(n);
+        }
+        public void MultiplicationTable(int value)
+        {
+            for (int i = 1; i <= value; i++)
+            {                
+                for (int j = 1; j <= value; j++)
+                {
+                    //product[j - 1] = (i * j).ToString();
+                    Console.Write("{0,4}", (i * j));
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void LowerCase(string line)
+        {
+            Console.WriteLine(line.ToLower());
+        }
+
+        public void DataRecovery(string line)
+        {
+            string[] subStr = line.Split(';');
+            Dictionary<string,string> wordsList = new Dictionary<string , string >();
+
+            wordsList.Add(subStr[1], subStr[0]);
+        }
+
+        public void LongestLines(string line)
+        {
+
+        }
+
+
+    }
+}
